860f9d8f1fb517930ec444a1ac4a5897d15265bb
GFontes008 <guilherme.fontes98@outlook.com>
2024-10-31 16:05:36 +0000

    redo

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,34 +1,59 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
-
-
-
+function maxOfTwoNumbers(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  } else num1 < num2;
+  {
+    return num2;
+  }
+}
 
 // Iteration 2 | Find the Longest Word
-const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
+const words = [
+  "mystery",
+  "brother",
+  "aviator",
+  "crocodile",
+  "pearl",
+  "orchard",
+  "crackpot",
+];
 
 function findLongestWord() {}
-
-
-
 
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
-
-
-
+function sumNumbers(numbers) {
+  if (numbers.lenght === 0) return 0;
+  return numbers.reduce((num, sum) => sum + num, 0);
+}
+console.log(sumNumbers(numbers));
 
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
-
-
-
+function averageNumbers(numbers) {
+  if (numbers.length === 0) return 0;
+  const sum = numbers.reduce((total, num) => total + num, 0);
+  return sum / numbers.length;
+}
 
 // Iteration 5 | Find Elements
-const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
+const words2 = [
+  "machine",
+  "subset",
+  "trouble",
+  "starting",
+  "matter",
+  "eating",
+  "truth",
+  "disobedience",
+];
+function doesWordExist(words, wordToFind) {
+  // Check if the array is empty, return null
+  if (words.length === 0) return null;
 
-function doesWordExist() {}
+  // Check if the word is in the array using includes, return true or false
+  return words.includes(wordToFind);
+}
